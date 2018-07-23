09ddc5a52458d03039bc995b09b53a110577b666
Voumo Tiogo Joel Brice <tiogoj@hotmail.com>
2018-07-23 23:15:18 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,11 +1,11 @@
 function forLoop(array[]){
   for (var i = 0; i < 25; i++) {
     if(i=0){
-      array+"I am ${i} strange loop${i}.";
+      array+`I am ${i} strange loop${i}.`;
     }
     else {
       {
-        array+"I am ${i} strange loops."
+        array+`I am ${i} strange loops.1`
       }
     }
   }
