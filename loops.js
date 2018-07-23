0a882dabf667dc05c4b350efe4236f4c1df1276b
Voumo Tiogo Joel Brice <tiogoj@hotmail.com>
2018-07-23 21:45:34 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,24 @@
+function forLoop(array[]){
+  for (var i = 0; i < 25; i++) {
+    if(i=0){
+      array+"I am ${i} strange loop${i}.";
+    }
+  }
+}
+function whileloop(n){
+  while(maybeTrue){
+    console.log(--n);
+  }
+  }
+function maybeTrue(){
+  Math.random()>=0.5;
+}
+doWhileLoop(array[]){
+  while (array[i].length>25) {
+    console.log(--array[i]);
+  }
+}
+do {
+    maybeTrue()
+
+} while (doWhileLoop(25));
